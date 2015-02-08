c0e0511798f715038599798ebd95782e3db6ab42
Will Filho <willxc2@gmail.com>
2015-02-08 16:29:15 -0200

    indentation fixed

--- Post-2(QtCore)/signal_emited.py
+++ Post-2(QtCore)/signal_emited.py
@@ -5,33 +5,33 @@ import sys
 
 
 class CustomLabel(QLabel):
-	def __init__(self, parent=None):
-		super(CustomLabel, self).__init__(parent)
-		self.setMouseTracking(True)
+    def __init__(self, parent=None):
+        super(CustomLabel, self).__init__(parent)
+        self.setMouseTracking(True)
 
-	def mousePressEvent(self, e):
-		img, re = QFileDialog.getOpenFileName(self,"Selecionar Arquivo",
-											filter="All(*.png *.jpg)")
-		if re:
-			self.setPixmap(QPixmap(img).scaled(250,150,
-							Qt.KeepAspectRatio))
+    def mousePressEvent(self, e):
+        img, re = QFileDialog.getOpenFileName(self,"Selecionar Arquivo",
+                                            filter="All(*.png *.jpg)")
+        if re:
+            self.setPixmap(QPixmap(img).scaled(250,150,
+                            Qt.KeepAspectRatio))
 
-	def mouseMoveEvent(self, e):
-		QApplication.setOverrideCursor(Qt.PointingHandCursor)
+    def mouseMoveEvent(self, e):
+        QApplication.setOverrideCursor(Qt.PointingHandCursor)
 
-	def leaveEvent(self, e):
-		QApplication.setOverrideCursor(Qt.ArrowCursor)
+    def leaveEvent(self, e):
+        QApplication.setOverrideCursor(Qt.ArrowCursor)
 
 class HandlerWindow(QWidget):
-	def __init__(self, parent=None):
-		super(HandlerWindow, self).__init__()
-		self.resize(300,350)
-		self.label = CustomLabel(self)
-		self.label.setPixmap(QPixmap("imgs/default-user.png").scaled(250,150,
-							Qt.KeepAspectRatio))
+    def __init__(self, parent=None):
+        super(HandlerWindow, self).__init__()
+        self.resize(300,350)
+        self.label = CustomLabel(self)
+        self.label.setPixmap(QPixmap("imgs/default-user.png").scaled(250,150,
+                            Qt.KeepAspectRatio))
 
 if __name__ == "__main__":
-	root = QApplication(sys.argv)
-	app = HandlerWindow()
-	app.show()
-	sys.exit(root.exec_())
+    root = QApplication(sys.argv)
+    app = HandlerWindow()
+    app.show()
+    sys.exit(root.exec_())
